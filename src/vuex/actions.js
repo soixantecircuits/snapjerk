5f7c5d6d8becd00380dddc86fb7c4abd4898c35a
kahaus <petitalx@gmail.com>
2016-08-18 21:25:57 +0200

    update save image file path

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -1,5 +1,7 @@
 const webcam = require('webcamjs')
 const fs = require('fs')
+const path = require('path');
+
 
 import settings from './../lib/settings.js'
 const starport = require('starport').default
@@ -14,14 +16,15 @@ export const takeSnap = function ({ dispatch, state }, data) {
     dispatch('ANS_TAKE_SNAP', dataURL)
 
     let base64Data = dataURL.replace(/^data:image\/jpeg;base64,/, "")
-    let file = settings.imageFolder + data.shortId + '-' + settings.flux + '.' + settings.cameraOptions.image_format
+    let filename = data.shortId + '-' + settings.flux + '.jpg'
+    let file = path.join(settings.imageFolder, filename)
     console.log('file:', file)
     fs.writeFile(file, base64Data, 'base64', function(err) {
       if (err !== null) {
         console.log(err)
       } else {
         let msg = {
-          src: 'mystaticserver:port/' + file,
+          src: 'http://' + path.join(settings.server.host + ':' + settings.server.port, filename),
           index: settings.flux,
           album: data.shortId
         }
