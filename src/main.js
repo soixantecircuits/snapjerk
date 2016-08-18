0d926b47f4e93a460aed8f97e2ebefc1cef3b3a3
kahaus <petitalx@gmail.com>
2016-08-18 14:28:18 +0200

    implement 'snap' events callback

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import settings from 'src/lib/settings.js'
 
 const App = Vue.extend({})
 
+// - webcam initialization
+
 const webcam = require('webcamjs')
 webcam.set(settings.cameraOptions)
 
@@ -29,5 +31,25 @@ function listDevices() {
     console.log(err.name + ": " + err.message)
   })
 }
+
+// - spacebro initialization
+
+const portconfig = settings.starport
+const starport = require('starport').default
+
+starport.connect({
+  server: {
+    address: portconfig.server.address,
+    port: portconfig.server.port
+  },
+  computer: portconfig.computer,
+  channel: portconfig.channel,
+  packers: [{ handler: data => console.log('=>', data) }],
+  unpackers: [{ handler: data => console.log('<=', data) }]
+})
+
+console.log('main.js')
+
+// - go  
 
 router.start(App, 'body')
