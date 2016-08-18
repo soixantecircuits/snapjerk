d8d85e54384ebd1039841a846abd954c595a7d46
kahaus <petitalx@gmail.com>
2016-08-18 20:52:15 +0200

    update image output location and emit 'image-saved' starpor event

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -2,17 +2,31 @@ const webcam = require('webcamjs')
 const fs = require('fs')
 
 import settings from './../lib/settings.js'
+const starport = require('starport').default
 
-export const takeSnap = function ({ dispatch, state }) {
+export const takeSnap = function ({ dispatch, state }, data) {
   dispatch('ASK_TAKE_SNAP')
+
+  console.log('datas: ', data.shortId)
 
   webcam.snap((dataURL) => {
     
     dispatch('ANS_TAKE_SNAP', dataURL)
 
     let base64Data = dataURL.replace(/^data:image\/jpeg;base64,/, "")
-    fs.writeFile(settings.file, base64Data, 'base64', function(err) {
-      console.log(err)
+    let file = settings.imageFolder + data.shortId + '-' + settings.flux + '.' + settings.cameraOptions.image_format
+    console.log('file:', file)
+    fs.writeFile(file, base64Data, 'base64', function(err) {
+      if (err !== null) {
+        console.log(err)
+      } else {
+        let msg = {
+          src: 'mystaticserver:port/' + file,
+          index: settings.flux,
+          album: data.shortId
+        }
+        starport.emit('image-saved', msg)
+      }
     })
   })
 }
