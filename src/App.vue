0e00161ffc35544f8e2345ec05a0908b1023ead9
kahaus <petitalx@gmail.com>
2016-08-18 13:29:10 +0200

    implement webcam in app vue component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,88 @@
 <template>
   <div id="app">
+    <button type='button' @click='takeSnap'>
+      <span>SNAP</span>
+    </button>
     <div id="live_canvas">LIVE CAMERA</div>
-    <img id="still_frame">
+    <img id="still_frame" v-bind:src="getDataUrl">
+    <!--
+    <div id="live_wrapper">
+      <div id="live_canvas">LIVE CAMERA</div>
+    </div>
+    <div id="still_wrapper">
+      <img id="still_frame">
+    </div>
+    -->
   </div>
 </template>
 
 <script>
-import store from './vuex/store'
+import store from 'src/vuex/store'
+import {getDataUrl} from 'src/vuex/getters'
+import {takeSnap} from 'src/vuex/actions'
+
+const webcam = require('webcamjs')
 
 export default {
   name: 'App',
-  store
+  store,
+  vuex: {
+    getters: {
+      getDataUrl
+    },
+    actions: {
+      takeSnap
+    }
+  },
+  ready() {
+    webcam.attach( '#live_canvas' )
+  }
 }
 </script>
 
 <style>
+
+/* reset css */
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+
 html {
   height: 100%;
 }
 
 body {
   display: flex;
-  align-items: center;
-  justify-content: center;
+  /* align-items: center; */
+  /* justify-content: center; */
   height: 100%;
 }
 
+button {
+  z-index: 1;
+  position: fixed;
+  top: 10px;
+  left: 10px;
+}
+
+/*
 #app {
   color: #2c3e50;
   margin-top: -100px;
@@ -49,5 +106,5 @@ body {
 .logo {
   width: 100px;
   height: 100px
-}
+}*/
 </style>
