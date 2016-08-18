34f7f795ab82edf84e6d34880c0fc17827787500
kahaus <petitalx@gmail.com>
2016-08-18 14:05:33 +0200

    save image on disk

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -1,8 +1,16 @@
-const webcam   = require('webcamjs')
+const webcam = require('webcamjs')
+const fs = require('fs')
 
 export const takeSnap = function ({ dispatch, state }) {
   dispatch('ASK_TAKE_SNAP')
+
   webcam.snap((dataURL) => {
+    
     dispatch('ANS_TAKE_SNAP', dataURL)
+
+    let base64Data = dataURL.replace(/^data:image\/jpeg;base64,/, "")
+    fs.writeFile("/tmp/out.jpg", base64Data, 'base64', function(err) {
+      console.log(err)
+    })
   })
 }
