fdbd654e6a2d25e055067bc2c525c0fb12610e9e
kahaus <petitalx@gmail.com>
2016-08-19 11:56:01 +0200

    use standard denomination for video devices

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import router from './router'
 import './transitions'
 import settings from 'src/lib/settings.js'
+import {listDevices, getDevicesMap} from 'src/lib/camUtils.js'
+
 const fs = require('fs')
 const path = require('path')
 const os = require("os")
@@ -11,69 +13,32 @@ const ip = require("ip")
 
 const App = Vue.extend({})
 
-// - webcam initialization
-
-const webcam = require('webcamjs')
-
-webcam.on( 'load', function() {
-  console.log('library is loaded')
-})
-
-webcam.on( 'live', function() {
-  console.log('camera is live, showing preview image (and user has allowed access)')
-})
-
-webcam.on( 'error', function(err) {
-  console.log('an error occurred ', err)
-})
-webcam.set(settings.cameraOptions)
-
-listDevices()
-
-function listDevices() {
-  if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
-    console.log("enumerateDevices() not supported.")
-    return
-  }
-
-  navigator.mediaDevices.enumerateDevices()
-  .then((devices) => {
-    devices.forEach((device) => {
-      // console.log(device.kind + ": " + device.label + " id = " + device.deviceId)
-      console.log("list devices: ", device)
-    })
-  })
-  .catch((err) => {
-    console.log(err.name + ": " + err.message)
-  })
-}
-
 // - spacebro initialization
 
 var snap = function (data, callback) {
-		webcam.snap((dataURL) => {
-        data.dataURL = dataURL
-				let base64Data = dataURL.replace(/^data:image\/jpeg;base64,/, "")
-				let filename = os.hostname() + '-' + data.shortId + '-' + settings.cameraNumber + '.jpg'
-				let file = path.join(settings.imageFolder, filename)
-				console.log('file:', file)
-				fs.writeFile(file, base64Data, 'base64', function(err) {
-					if (err !== null) {
-						console.log(err)
-						typeof callback === 'function' && callback(err, data)
-					} else {
-            if (settings.server.host == "") settings.server.host = undefined 
-            let host = (settings.server.host || ip.address())
-						let msg = {
-							src: 'http://' + path.join( host  + ':' + settings.server.port, filename),
-							number: settings.cameraNumber,
-							album_name: data.shortId || Math.random()*10000
-						}
-						spacebroClient.emit('image-saved', msg)
-					}
-				})
-				typeof callback === 'function' && callback(null, data)
-			})
+	webcam.snap((dataURL) => {
+    data.dataURL = dataURL
+		let base64Data = dataURL.replace(/^data:image\/jpeg;base64,/, "")
+		let filename = os.hostname() + '-' + data.shortId + '-' + settings.cameraNumber + '.jpg'
+		let file = path.join(settings.imageFolder, filename)
+		console.log('file:', file)
+		fs.writeFile(file, base64Data, 'base64', function(err) {
+			if (err !== null) {
+				console.log(err)
+				typeof callback === 'function' && callback(err, data)
+			} else {
+        if (settings.server.host == "") settings.server.host = undefined 
+        let host = (settings.server.host || ip.address())
+				let msg = {
+					src: 'http://' + path.join( host  + ':' + settings.server.port, filename),
+					number: settings.cameraNumber,
+					album_name: data.shortId || Math.random()*10000
+				}
+				spacebroClient.emit('image-saved', msg)
+			}
+		})
+		typeof callback === 'function' && callback(null, data)
+	})
 }
 var spacebroClient = require('spacebro-client')
 var actionList = [
@@ -115,7 +80,41 @@ var server = http.createServer(function (req, res) {
 
 server.listen(settings.server.port)
 
-// - go  
+// - webcam initialization
 
-router.start(App, 'body')
+const webcam = require('webcamjs')
+
+webcam.on( 'load', function() {
+  console.log('library is loaded')
+})
+
+webcam.on( 'live', function() {
+  console.log('camera is live, showing preview image (and user has allowed access)')
+})
+
+webcam.on( 'error', function(err) {
+  console.log('an error occurred ', err)
+})
+
+getDevicesMap(onDeviceMapCreated)
+
+function onDeviceMapCreated(err, res) {
+  console.log(res, err)
+  if (res[settings.device]) {
+    let id = res[settings.device]
+    let cameraOptions 
+    cameraOptions = Object.assign({}, settings.cameraOptions)
+    cameraOptions.constraints.optional = [{
+      "sourceId": id
+    }]
+    webcam.set(cameraOptions)
+  } else {
+    console.error('there is no flux for : ', settings.device)
+  }
+
+  // - go  
+
+  router.start(App, 'body')
+}
+
 
